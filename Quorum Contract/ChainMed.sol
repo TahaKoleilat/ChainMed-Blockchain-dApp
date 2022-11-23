fef5fa7df82414476186e8a60aeeaf0134406ce0
TahaKoleilat <tok00@mail.aub.edu>
2022-11-23 13:15:52 +0200

    integrating updates

--- Quorum Contract/ChainMed.sol
+++ Quorum Contract/ChainMed.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 contract ChainMed{
 
-    address Admin = address(0);
+    address public constant Admin = address(0xed9d02e382b34818e88B88a309c7fe71E65f419d);
     // This declares a new complex type which will
     // be used for variables later.
     // It will represent a single patient
@@ -13,6 +13,8 @@ contract ChainMed{
         uint256 filesAllowed;   
         // represents the public key in the blockchain for the patient
         address patientAddress; 
+        // represents the public key of the doctor which will be used for asymmetric encryption
+        string patientPublicKey;
     }
 
     struct Doctor {
@@ -23,14 +25,16 @@ contract ChainMed{
         uint256 doctorID;
         // represents the public key in the blockchain for the doctor
         address doctorAddress; 
+        // represents the public key of the doctor which will be used for asymmetric encryption
+        string doctorPublicKey;
     }
 
     mapping(bytes32 => Patient) public patientMap; //add patient instance to dictionary indexed by a 256 bit hash
     mapping(bytes32 => Doctor) public doctorMap; //dictionary for all registered doctors in the healthcare organization
-    mapping(bytes32=>bytes32[]) public ipfsMap; //Hash of IPFS File stored in encrypted form
+    mapping(bytes32=>string[]) public ipfsMap; //Hash of IPFS File stored in encrypted form
     
-    constructor() {
-        require(msg.sender == Admin, "You are not authorized to deploy a contract!");
+    constructor(){
+        require(msg.sender == Admin, "You are not authorized to deploy a contract");
     }
     
     function registerDoctor(uint256 _doctorID,string memory _doctorName,string memory _doctorSurname, address _doctorAddress) external{
@@ -43,28 +47,31 @@ contract ChainMed{
     function registerPatient(uint256 _patientID,address _patientAddress) external{
         require(msg.sender == Admin, "You are not authorized!");
         require(patientMap[sha256(abi.encodePacked(_patientAddress))].patientAddress == address(0), "Patient is already registered!");
-        patientMap[sha256(abi.encodePacked(_patientID,_patientAddress))] = Patient({patientID: _patientID,filesAllowed: 0,
+        patientMap[sha256(abi.encodePacked(_patientAddress))] = Patient({patientID: _patientID,filesAllowed: 0,
         patientAddress:_patientAddress});
     }
 
-    function uploadFile(bytes32 _ipfshash,address _doctorAddress,address _patientAddress) external{
+    function uploadFile(string memory _ipfshash,address _doctorAddress,address _patientAddress) external{
         require(msg.sender == _patientAddress || msg.sender == _doctorAddress, "Unauthorized identity!");
         require(patientMap[sha256(abi.encodePacked(_patientAddress))].patientID != 0, "Patient is not registered!");
         require(doctorMap[sha256(abi.encodePacked(_doctorAddress))].doctorID != 0, "Doctor is not registered!");
+        if(msg.sender == _patientAddress){
+            require(patientMap[sha256(abi.encodePacked(_patientAddress))].filesAllowed > 0, "You can't upload more files!");
+            patientMap[sha256(abi.encodePacked(_patientAddress))].filesAllowed--;
+        }
         ipfsMap[sha256(abi.encodePacked(_patientAddress,_doctorAddress))].push(_ipfshash);
     }
 
-    function retrieveFile(address _doctorAddress,address _patientAddress) public view returns (bytes32[] memory){
-        require(msg.sender == _patientAddress || msg.sender == _doctorAddress, "Unauthorized identity!");
+    function retrieveFile(address _doctorAddress,address _patientAddress) public view returns (string[] memory){
         require(patientMap[sha256(abi.encodePacked(_patientAddress))].patientID != 0, "Patient is not registered!");
         require(doctorMap[sha256(abi.encodePacked(_doctorAddress))].doctorID != 0, "Doctor is not registered!");
         return(ipfsMap[sha256(abi.encodePacked(_patientAddress,_doctorAddress))]);
     }
 
     function setFileAllowed(address _doctorAddress,address _patientAddress,uint256 _filesAllowed) external{
+        require((doctorMap[sha256(abi.encodePacked(_doctorAddress))]).doctorID != 0, "Doctor is not registered!");
         require(msg.sender == _doctorAddress || msg.sender == Admin, "You are not authorized!");
         require((patientMap[sha256(abi.encodePacked(_patientAddress))]).patientID != 0, "Patient is not registered!");
-        require((doctorMap[sha256(abi.encodePacked(_doctorAddress))]).doctorID != 0, "Doctor is not registered!");
         patientMap[sha256(abi.encodePacked(_patientAddress))].filesAllowed = _filesAllowed;
     }
 
@@ -79,5 +86,14 @@ contract ChainMed{
         require((doctorMap[sha256(abi.encodePacked(_doctorAddress))]).doctorID != 0, "No such entity exists!");
         delete doctorMap[sha256(abi.encodePacked(_doctorAddress))];
     }
-
+    
+    function getPublicKey(address _address) public view returns (string memory){
+        require(patientMap[sha256(abi.encodePacked(_address))].patientID != 0 || doctorMap[sha256(abi.encodePacked(_address))].doctorID != 0, "No such entity exists!");
+        if(patientMap[sha256(abi.encodePacked(_address))].patientID != 0){
+            return patientMap[sha256(abi.encodePacked(_address))].patientPublicKey;
+        }
+        else{
+            return doctorMap[sha256(abi.encodePacked(_address))].doctorPublicKey;
+        }
+    }
 }
